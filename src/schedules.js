0942886141d2d1a96ecad3c77f47a160990c6905
Terran Blake <terranblake@gmail.com>
2019-12-29 09:13:01 -0800

    migrate to new pubsub package name

--- src/schedules.js
+++ src/schedules.js
@@ -5,22 +5,54 @@ const models = require('@postilion/models');
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
+const day = hour * 24;
 
 module.exports = [
 	{
 		// keep filings updated by finding all companies
 		// who were last synced more than 30 minutes ago
-		name: 'SyncAllCompanyFilings',
 		queue: 'SyncFilingsByTicker',
 		model: models.Company,
 		query: {
-			'stats.lastSyncedFilingsAt': {
-				$lt: moment().subtract(30, 'minute').toDate()
-			}
+			$or: [
+				{
+					'stats.lastSyncedFilingsAt': {
+						$lt: moment().subtract(30, 'minute').toDate()
+					}
+				},
+				{
+					'stats.lastSyncedFilingsAt': null
+				}
+			]
 		},
 		options: {
 			repeat: {
 				every: hour,
+				limit: 1000
+			}
+		}
+	},
+	{
+		// keep earnings calendar up-to-date with integrations
+		// to ensure that we are fetching filings as soon as
+		// they are published and are downloadable
+		queue: 'SyncCompanyEarningsEvents',
+		model: models.Company,
+		query: {
+			$or: [
+				{
+					'stats.lastSyncedEarningsAt': {
+						$lt: moment().subtract(1, 'days').toDate()
+					}
+				},
+				{
+					'stats.lastSyncedEarningsAt': null
+				}
+			]
+		},
+		options: {
+			repeat: {
+				every: day,
 				limit: 1000
 			}
 		}
--- src/service.js
+++ src/service.js
@@ -1,16 +1,16 @@
-const { Queue } = require('@postilion/event-framework');
+const { Queue } = require('@postilion/pubsub');
 const { logger } = require('@postilion/utils');
 
 const schedules = require('./schedules');
 
 async function createJobsFromSchedules() {
 	for (let schedule of schedules) {
-		const { name, model, query, queue } = schedule;
+		const { model, query, queue } = schedule;
 
 		const scheduleQueue = new Queue(queue);
 
 		const documents = await model.find(query);
-		logger.info(`creating ${documents.length} jobs for schedule ${name} for model ${model.modelName} for queue ${queue}`);
+		logger.info(`creating ${documents.length} jobs for queue ${queue} for model ${model.modelName}`);
 
 		for (let document of documents) {
 			scheduleQueue.add(document);
